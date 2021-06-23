79fafdfc52db111a04f0917587e07a9ec412a486
Daniel Lwetabe <dannylwe11@gmail.com>
2021-06-23 22:26:11 +0300

    spotify init

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div className="login">
+            <h1>Login</h1>
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-login" />
+        </div>
+    )
+}
+
+export default Login
