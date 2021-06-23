d36f0e046f1b96f717b46a11b680d060c95e9837
Daniel Lwetabe <dannylwe11@gmail.com>
2021-06-23 22:51:28 +0300

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Login() {
     return (
         <div className="login">
-            <h1>Login</h1>
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-login" />
+            <a href="#">Login with Spotify</a>
         </div>
     )
 }
